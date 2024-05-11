13e4234dba73fb816a986d0b809ac27e1ae1962b
hpmcreative <162780459+hpmcreative@users.noreply.github.com>
2024-05-11 11:47:44 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -658,7 +658,7 @@ async def send_all(bot, userid, files, ident):
                         InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                         InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                     ],[
-                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/creatorbeatz")
+                        InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/Mallu_adminbot")
                         ]
                     ]
                 )
