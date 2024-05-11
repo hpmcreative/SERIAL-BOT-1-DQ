a3faf62605a421945445b670b0bbc8794d746d31
hpmcreative <162780459+hpmcreative@users.noreply.github.com>
2024-05-11 12:20:43 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1437,7 +1437,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "owner_info":
             btn = [[
                     InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="start"),
-                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/creatorbeatz")
+                    InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="t.me/Mallu_adminbot")
                   ]]
             await client.edit_message_media(
                 query.message.chat.id, 
